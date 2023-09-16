a1adf6f344a70debdae8826b20d9eea77c887968
Christian Stankowic <info@cstan.io>
2023-09-16 12:17:28 +0200

    make build script run on RPM-like distros

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python3 build.py
-docker container run -v $(pwd):/slides -it docker.io/astefanutti/decktape presentation.html presentation.pdf --chrome-arg=--disable-web-security
+docker container run -v $(pwd):/slides:z -it docker.io/astefanutti/decktape presentation.html presentation.pdf --chrome-arg=--disable-web-security
